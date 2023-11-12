5c445593ca56979158d3ac1db3e647d2829a92a6
mgongalsky <54280807+mgongalsky@users.noreply.github.com>
2023-11-13 01:06:03 +0300

    First commit

--- admin/local.py
+++ admin/local.py
@@ -1,9 +1,9 @@
 class EnvironmentSettings:
     def __init__(self):
-        self.workspace_dir = './'
+        self.workspace_dir = ''    # Base directory for saving network checkpoints.
         self.tensorboard_dir = self.workspace_dir + '/tensorboard/'    # Directory for tensorboard files.
-        self.pretrained_nets_dir = '/hdd/burst_SR/deep-burst-sr-master-L1-BasicVSRpp-shiyanshijiqun/pretrained_networks'    # Directory for pre-trained networks.
-        self.save_data_path = self.workspace_dir + '/evaluation'    # Directory for saving network predictions for evaluation.
-        self.zurichraw2rgb_dir = '../dataset/Zurich-RAW-to-DSLR-Dataset'    # Zurich RAW 2 RGB path
-        self.burstsr_dir = '../dataset/burstsr_dataset'    # BurstSR dataset path
-        self.synburstval_dir = '../dataset/syn_burst_val'    # SyntheticBurst validation set path
+        self.pretrained_nets_dir = 'E:\VR\Experiments\python\RBSR\admin\../pretrained_networks/'    # Directory for pre-trained networks.
+        self.save_data_path = ''    # Directory for saving network predictions for evaluation.
+        self.zurichraw2rgb_dir = ''    # Zurich RAW 2 RGB path
+        self.burstsr_dir = './my_dataset/burstsr_dataset'    # BurstSR dataset path
+        self.synburstval_dir = 'E:\VR\Experiments\python\RBSR\SyntheticBurstVal'    # SyntheticBurst validation set path
